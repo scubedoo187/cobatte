be1c342e8e19ea620a8f8b57e066422007f49c0e
Jung <sublime718@gmail.com>
2014-04-09 18:40:53 +0900

    test

--- test.java
+++ test.java
@@ -7,15 +7,9 @@ class test{
 		c = a + b;
 
 		System.out.println("sum of : %d", c);
-<<<<<<< HEAD
-<<<<<<< HEAD
 		System.out.println("stop this program");
-=======
-=======
 		System.out.println("out of bound");
->>>>>>> 8d0fbf8947d67757e401669ef182eb1a8ed2919b
 
 
->>>>>>> ac972b60981f32c21d58de26535bbe115d9f26b1
 	}
 }
