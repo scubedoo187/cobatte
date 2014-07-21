16e8b259d4802915973fad62f1377d917beab670
Jung <sublime718@hotmail.com>
2014-07-21 17:32:59 +0900

    7/21 first client modi

--- TaxiClient/Taxi/Taxi/gen/com/cobatte/taxi/R.java
+++ TaxiClient/Taxi/Taxi/gen/com/cobatte/taxi/R.java
@@ -38,9 +38,14 @@ public final class R {
     public static final class drawable {
         public static final int hallym=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int intro=0x7f020002;
-        public static final int intro_2=0x7f020003;
-        public static final int taxi_icon=0x7f020004;
+        public static final int icon=0x7f020002;
+        public static final int icon_2=0x7f020003;
+        public static final int icon_3=0x7f020004;
+        public static final int icon_4=0x7f020005;
+        public static final int intro=0x7f020006;
+        public static final int intro_2=0x7f020007;
+        public static final int intro_3=0x7f020008;
+        public static final int taxi_icon=0x7f020009;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0024;
