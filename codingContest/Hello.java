fd9b232a85d772a94952487e0124f6dd8096c948
Im <gotjd1emd@gmail.com>
2015-05-04 15:50:17 +0900

    Delete garbage

--- codingContest/Hello.java
+++
@@ -1,8 +0,0 @@
-// 2014_4_10 console Hello world program
-// Edited by YG_Jung
-
-class Hello{
-	public static void main(String args[]){
-		System.out.println("Hello world");
-	}
-}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int i;
-	for( i=0; i<4; i++ )
-		printf("Hello C\n");
-	return 0;
-}
